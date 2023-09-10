3743d7b7e81643f876f1911c8a895482ad1c45d3
esraa mosaad <esraa.m.mosaad@gmail.com>
2023-09-10 16:53:10 +0300

    upload project

---
+++ lib/widgets/login_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+
+    );
+  }
+}
